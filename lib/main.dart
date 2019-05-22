99c355054fbecc36b10d411d5f7f057c03e9667d
ogomb <lewis.mbogo@andela.com>
2019-05-22 11:16:49 +0300

    create first screen for the app.

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+      ),
+    );
+  }
+}
